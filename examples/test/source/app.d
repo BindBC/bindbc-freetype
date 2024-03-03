4c1b39c6980ca22da5d0963b1e7f676bb596f1be
IchorDev <15670465+ichordev@users.noreply.github.com>
2024-03-03 21:20:54 +0700

    Improve 'test' example

--- examples/test/source/app.d
+++ examples/test/source/app.d
@@ -13,4 +13,35 @@ void main(){
 			return;
 		}
 	}
+	FT_Library ftLib;
+	FT_Error err;
+	err = FT_Init_FreeType(&ftLib);
+	if(err){
+		printf("Loading error: %s", FT_Error_String(err));
+	}
+	
+	int maj, min, pat;
+	FT_Library_Version(ftLib, &maj, &min, &pat);
+	assert(maj >= 2);
+	assert(min >= 13);
+	
+	FT_Face ftFace;
+	enum fontPath = {
+		version(linux)        return "/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf";
+		else version(OSX)     static assert(0, "No font path set for macOS"); //return "";
+		else version(Windows) static assert(0, "No font path set for Windows"); //return "";
+		else static assert(0, "No font path for this operating system.");
+	}();
+	FT_New_Face(ftLib, fontPath, 0, &ftFace);
+	assert(ftFace !is null, "FreeType couldn't load font");
+	
+	err = FT_Load_Glyph(ftFace, FT_Get_Char_Index(ftFace, 'A'), FT_LOAD_DEFAULT);
+	if(err){
+		printf("Loading error: %s", FT_Error_String(err));
+	}
+	
+	FT_GlyphSlot_Embolden(ftFace.glyph);
+	
+	FT_Done_Face(ftFace);
+	FT_Done_FreeType(ftLib);
 }
